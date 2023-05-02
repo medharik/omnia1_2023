d2b334349d578c42d17175184a07d6b2f38d0772
medharik <med.harik@gmail.com>
2023-05-02 13:14:18 +0100

    les tableau en JS

--- js_algo/s8/test1.js
+++ js_algo/s8/test1.js
@@ -1,2 +1,20 @@
-console.log("mom premier message js par nodejs")
-console.log('version 2')
+// les tableaux en javascript
+let t1=[];
+let  t2=[1,3,7,-9];
+let a=[1,3,7,-9,"salut"];
+// a[10]="ali";
+a.push(100);
+a.push("ali");
+a.pop();
+a.pop();
+a.pop();
+a.shift()
+a.unshift("debut");
+let k=[10,3,8,0,100,700,-390,66,55];
+r=k.slice(4,7);
+console.log('le resultat est : ',r);
+let sp=[1,2,3,4,5];
+let x=sp.splice(0,2);
+console.log('x est ',sp)
+// console.log(a[3],a[1],a[10],a.length);
+console.log(a);
