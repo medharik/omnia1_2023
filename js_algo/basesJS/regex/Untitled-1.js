8f63161573ecf5317d9a19f9ab88cda88ff02851
medharik <med.harik@gmail.com>
2023-05-09 10:11:49 +0100

    REGEX exemple de CIN

---
+++ js_algo/basesJS/regex/Untitled-1.js
@@ -0,0 +1,23 @@
+les expressions regulieres :
+interval: 
+[a-z] : lettre
+[0-9] : chiffre
+[a-z0-9] : alphanumerique
+[a-z.@/+] : lettre de a à z et .@/+
+
+quantificateur : 
+cin : bj123451, ba123877, a12345
+[a-zA-Z]{1,2}[0-9]{5,6}
+en javascript :
+let modelCin =/[a-zA-Z]{1,2}[0-9]{5,6}/
+let cin ="bj123456";
+if (modelCin.test(cin)) {
+    alert("CIn correct")
+}else{
+    alert("CIn incorrect");
+}
+
+
+1A1
+12A12324
+1B123
